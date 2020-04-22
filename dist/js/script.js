0a423f3f73d6b51bac3d01b48331d4a92086de95
Vyacheslav <beeeshop@yandex.ru>
2020-04-22 09:11:08 +0400

    all section done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
   $('.carousel').slick({
     infinite: true,
-        speed: 1000,
-        autoplay: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        //  responsive: [
-        //     {
-        //         breakpoint: 991,
-        //         settings: {
-        //             dots: true,
-        //             arrows: false
-        //         }
+    speed: 1000,
+    autoplay: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    //  responsive: [
+    //     {
+    //         breakpoint: 991,
+    //         settings: {
+    //             dots: true,
+    //             arrows: false
+    //         }
 
-        //     }
-        // ]
+    //     }
+    // ]
   });
 });
